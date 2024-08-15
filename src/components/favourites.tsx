5f5b36630ff3fd97d4062dd6a1062477d6e25c2d
Anusree-mj <anuanusree682@gmail.com>
2024-08-15 22:15:41 +0530

    feat:fetch quotes, style:favourites

--- src/components/favourites.tsx
+++ src/components/favourites.tsx
@@ -1,9 +1,41 @@
-import { Box } from "@mui/material"
-
+import { Box, Typography } from "@mui/material"
+import CloseIcon from '@mui/icons-material/Close';
+import { useNavigate } from "react-router-dom";
 
 const FavouritesComponent = () => {
+  const navigate = useNavigate()
+
   return (
-    <Box sx={{ color: 'black' }} >FavouritesComponent</Box >
+    <Box sx={{
+      width: '30rem', maxWidth: '95%', minHeight: '40vh',
+      display: 'flex', flexDirection: 'column',
+      alignItems: 'center', justifyContent: 'center',
+      background: 'linear-gradient(to left, #1b9b96, #2e6f79)',
+      p: 2, overflow: 'hidden'
+
+    }}>
+      <Box sx={{
+        maxWidth: '100%', width: '30rem', mb: 2,
+        display: "flex", alignItems: 'flex-start',
+        justifyContent: 'space-between',
+      }}>
+        <Typography sx={{
+          fontSize: '1.3rem',
+          fontWeight: '800'
+        }}>Favourites</Typography>
+        <CloseIcon sx={{
+          color: 'white',
+          backgroundColor: '#00000052', p: '0.2rem',
+          cursor: 'pointer'
+
+        }} onClick={() => navigate('/')} />
+      </Box>
+      <Box sx={{
+        width: '100%',minHeight:'25vh',
+      }}>
+      
+      </Box>
+    </Box >
   )
 }
 
--- src/components/quote.tsx
+++ src/components/quote.tsx
@@ -1,23 +1,44 @@
 import { Box, Button, Typography } from '@mui/material'
 import FavoriteSharpIcon from '@mui/icons-material/FavoriteSharp';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import FormatQuoteIcon from '@mui/icons-material/FormatQuote';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const QuoteComponent = () => {
     const [quote, setQuote] = useState({
-        text: 'quoteee',
-        author: 'author'
+        text: '',
+        author: ''
     })
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        getQuotes()
+    }, [])
+
+    const getQuotes = async () => {
+        try {
+            const { data } = await axios.get('https://type.fit/api/quotes');
+            const randomQuote = data[Math.floor(Math.random() * data.length)];
+            setQuote({
+                text: randomQuote.text,
+                author: randomQuote.author
+            })
+        }
+        catch (err) {
+            console.log(err)
+        }
+    }
     return (
         <Box sx={{
-            width: '30rem', maxWidth: '95%',
+            width: '30rem', maxWidth: '95%',minHeight:'40vh',
             display: 'flex', flexDirection: 'column',
             alignItems: 'center', justifyContent: 'center',
             backgroundColor: '#212226', p: 2, overflow: 'hidden'
 
         }}>
             <Box sx={{
-                maxWidth: '100%', width: '30rem',
+                maxWidth: '100%', width: '30rem', mb: 2,
                 display: "flex", alignItems: 'center',
                 justifyContent: 'space-between'
             }}>
@@ -30,7 +51,7 @@ const QuoteComponent = () => {
                     backgroundColor: '#00000052', p: '0.2rem',
                     cursor: 'pointer'
 
-                }} />
+                }}  onClick={() => navigate('/favourites')}/>
             </Box>
             <Box sx={{
                 display: 'flex', flexDirection: 'column',
@@ -42,7 +63,7 @@ const QuoteComponent = () => {
                 }} />
                 <Typography sx={{ textAlign: 'start', ml: 5, mt: 2, mb: 2 }}>{quote.text}</Typography>
                 <Typography sx={{ textAlign: 'end', mb: 2, mr: 5 }}>- {quote.author}</Typography>
-                <FormatQuoteIcon sx={{ alignSelf: 'flex-end' }} />
+                <FormatQuoteIcon sx={{ alignSelf: 'flex-end', cursor: 'pointer' }} />
                 {/* <Box sx={{
                     position: 'absolute', borderRadius: '50%'
                 }}>
@@ -54,13 +75,14 @@ const QuoteComponent = () => {
                     </Box>
                 </Box> */}
                 <Box sx={{
-                    display: 'flex', mt: 2,
+                    display: 'flex', mt: 2, width: '30rem', maxWidth: '90%',
                     alignItems: 'center', justifyContent: 'space-around',
                 }}>
                     <Button variant="contained" sx={{
                         background: 'linear-gradient(to left, #553ddd, #7f69e8)',
                         borderRadius: '0.4rem', width: '10rem', p: '4px'
-                    }}>New Quote</Button>
+                    }} onClick={getQuotes}
+                    >New Quote</Button>
                     <Button variant="contained" sx={{
                         background: 'linear-gradient(to left, #2e6f79, #1b9b96)',
                         borderRadius: '0.4rem', width: '10rem', p: '4px'
